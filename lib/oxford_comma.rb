d71b171ae28e5d9c26891dea23376086008953e8
James Ferguson <ncjamesferguson@gmail.com>
2017-11-30 22:52:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,13 @@
 def oxford_comma(array)
 
   case array
-
-  when array.length = 1
-    array.first
-  when array.length = 2
-    array.join(" and ")
-  when array.length > 2
-    closer = array.pop
-    array.join(", ") + "and #{closer}"
+    when array.length = 1
+      array.first
+    when array.length = 2
+      array.join(" and ")
+    when array.length > 2
+      closer = array.pop
+      array.join(", ") + "and #{closer}"
   end
 
 end
