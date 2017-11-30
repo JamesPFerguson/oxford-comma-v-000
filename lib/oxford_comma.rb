b9bb1e7663f24137cb6c4e09944bedaffa021bb9
James Ferguson <ncjamesferguson@gmail.com>
2017-11-30 22:51:06 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
 
+  case array
+
+  when array.length = 1
+    array.first
+  when array.length = 2
+    array.join(" and ")
+  when array.length > 2
+    closer = array.pop
+    array.join(", ") + "and #{closer}"
+  end
+
 end
